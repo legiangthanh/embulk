5154d45c40c95f77a89a24bcc69f987d5669091b
Thanh Le <legiangthanh@gmail.com>
2023-03-22 13:49:04 +0700

    add TaskReport To ExecutionResult and new SPI for parser

--- embulk-core/src/main/java/org/embulk/exec/ExecutionResult.java
+++ embulk-core/src/main/java/org/embulk/exec/ExecutionResult.java
@@ -1,17 +1,29 @@
 package org.embulk.exec;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import org.embulk.config.ConfigDiff;
+import org.embulk.config.TaskReport;
 
 public class ExecutionResult {
     private final ConfigDiff configDiff;
     private final boolean skipped;
     private final List<Throwable> ignoredExceptions;
+    private List<TaskReport> inputTaskReports = new ArrayList<>();
+    private List<TaskReport> outputTaskReports = new ArrayList<>();
 
     public ExecutionResult(ConfigDiff configDiff, boolean skipped, List<Throwable> ignoredExceptions) {
         this.configDiff = configDiff;
         this.skipped = skipped;
         this.ignoredExceptions = ignoredExceptions;
+    }
+
+    public ExecutionResult(ConfigDiff configDiff, boolean skipped, List<Throwable> ignoredExceptions,
+                           List<TaskReport> inputTaskReports, List<TaskReport> outputTaskReports) {
+        this(configDiff, skipped, ignoredExceptions);
+        this.inputTaskReports = inputTaskReports;
+        this.outputTaskReports = outputTaskReports;
     }
 
     public ConfigDiff getConfigDiff() {
@@ -24,5 +36,13 @@ public class ExecutionResult {
 
     public List<Throwable> getIgnoredExceptions() {
         return ignoredExceptions;
+    }
+
+    public List<TaskReport> getOutputTaskReports() {
+        return outputTaskReports;
+    }
+
+    public List<TaskReport> getInputTaskReports() {
+        return inputTaskReports;
     }
 }
